ca43c145c58b2c1bc7ed2a39416775bf496855fa
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-06-09 11:52:44 +0200

    basic understanding of the 'new' thing so bascily adding new values to the object so we can do things with it

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("test");
+const theHobbit = new Books('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
+//so! use 'new' to put in the values in the object of you're choise! 
+
+function Books(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function(){
+        return title + ' by ' + author + ', ' + pages + ', '+ read;
+    }
+}
+
+console.log(theHobbit.info()); // logs "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
