25be18d39e24bcc23bf73c3991447e9f4c7e8d05
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-06-19 13:30:54 +0200

    pushing form data into object

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
-const HiddenForm = document.querySelector('.HiddenForm'); //werkt
-const Library = document.querySelector('.Library'); //werkt
+const form = document.querySelector('form');
+const Title = document.getElementById('Title');
+const author = document.getElementById('author');
+const Pages = document.getElementById('Pages');
+const Library = document.getElementById('Library');
+const SubmitBook = document.getElementById('SubmitBook');
 
-function LibraryDisappear(){
-    console.log("aardappel");
-}
+const theHobbit = new Books('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
+console.log(theHobbit.info()); // logs "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
 
-function SubmitBookDissapear(){
-    console.log("aardappel");
-    const Title =  document.getElementById('Title');
-    const author = document.getElementById('author');
-    const Pages = document.getElementById('Pages');
-    const Read = document.getElementById('Read');
-    console.log(Title.value + " this the title b");
-    console.log(author.value + " this the author b");
-    console.log(Pages.value + " this the Pages b");
-    console.log(Read.value + " this the Read  b");
-}
-/*
-
-const AddBook = new Books(Title.value, author.value, Pages.value, Read.value); 
-
+//constructor object
 function Books(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
     this.info = function(){
-        const show = document.querySelector('show');
-        const Row1 = document.createElement('tr');
-
-        //push the information here to a new table-row
-        //return title + ' by ' + author + ', ' + pages + ', '+ read;
+        return title + ' by ' + author + ', ' + pages + ', '+ read;
     }
 }
 
-console.log(Book1.info()); // logs "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+//adding a book to the object
+function AddBook(){
+    SubmitBook.addEventListener('click', () => {
+        document.getElementById('HiddenForm').style.visibility = 'visible';
+        document.getElementById('Library').style.visibility = 'hidden';
+    });
 
-*/
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const myFormData =  new FormData(e.target);
+
+        const myLibrary = Object.fromEntries(myFormData.entries());
+        console.log(myLibrary.Title);
+
+        document.getElementById('HiddenForm').style.visibility = 'hidden';
+        document.getElementById('Library').style.visibility = 'visible';
+    });
+}
+
+AddBook();
